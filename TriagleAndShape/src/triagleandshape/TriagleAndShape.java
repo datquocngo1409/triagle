364778cb1d3167532c4740c7bfef2caa12f5cca5
NgoQuocDat <shaco14091999@gmail.com>
2018-10-27 11:16:53 +0700

    add

---
+++ TriagleAndShape/src/triagleandshape/TriagleAndShape.java
@@ -0,0 +1,10 @@
+package triagleandshape;
+
+public class TriagleAndShape {
+
+    public static void main(String[] args) {
+        Triagle t = new Triagle(3, 4, 5);
+        System.out.println(t.toString());
+    }
+    
+}
